1061fc6789c3e5dbf70dbd7cf09e6b76226cf0b0
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:56:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exceptions1TryCatchFinally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of program");
+            }
+        }
+    }
+}
